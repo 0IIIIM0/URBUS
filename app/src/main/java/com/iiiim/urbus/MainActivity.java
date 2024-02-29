71259701c068f9cd9af93685789205ac2bba79ef
Ignacio Medina <ignac23mde@gmail.com>
2024-02-29 11:09:18 -0600

    Initial commit

---
+++ app/src/main/java/com/iiiim/urbus/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iiiim.urbus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
